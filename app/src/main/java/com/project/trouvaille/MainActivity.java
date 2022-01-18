fd7edb8da85b678b5cde3bdedd5781a71374b205
Utsav <utsavdevadiga10@gmail.com>
2022-01-18 23:18:59 +0530

    Added a sign-up page ui sketch

--- app/src/main/java/com/project/trouvaille/MainActivity.java
+++ app/src/main/java/com/project/trouvaille/MainActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.color.DynamicColors;
 
 public class MainActivity extends AppCompatActivity {
     Button dummyLogin;
+    TextView member;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         DynamicColors.applyToActivitiesIfAvailable(getApplication());
         dummyLogin = findViewById(R.id.dummy_login);
+        member = findViewById(R.id.not_a_member);
+
+        member.setOnClickListener(view -> {
+            Intent Signup = new Intent(this, SignupActivity.class);
+            startActivity(Signup);
+            finish();
+        });
 
 
         dummyLogin.setOnClickListener(view -> {
             Intent homeIntent = new Intent(this, HomeActivity.class);
             startActivity(homeIntent);
+            finish();
         });
 
     }
---
+++ app/src/main/java/com/project/trouvaille/SignupActivity.java
@@ -0,0 +1,38 @@
+package com.project.trouvaille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.color.DynamicColors;
+
+public class SignupActivity extends AppCompatActivity {
+    Button signup_dummy;
+    TextView already_member;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        DynamicColors.applyToActivitiesIfAvailable(getApplication());
+        signup_dummy = findViewById(R.id.dummy_signup);
+        already_member = findViewById(R.id.already_a_member);
+
+        signup_dummy.setOnClickListener(view -> {
+            Intent SignupIntent = new Intent(this, HomeActivity.class);
+            startActivity(SignupIntent);
+            finish();
+        });
+
+
+        already_member.setOnClickListener(view -> {
+            Intent loginIntent = new Intent(this, SignupActivity.class);
+            startActivity(loginIntent);
+            finish();
+        });
+    }
+}
