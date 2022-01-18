6a6e1b74e54cfcc71e57374bdaa1b4451c7bd138
Utsav <utsavdevadiga10@gmail.com>
2022-01-18 22:53:03 +0530

    added a new activity and UI updates

--- app/src/main/java/com/project/trouvaille/MainActivity.java
+++ app/src/main/java/com/project/trouvaille/MainActivity.java
@@ -3,14 +3,26 @@ package com.project.trouvaille;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.color.DynamicColors;
+
 public class MainActivity extends AppCompatActivity {
+    Button dummyLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+        DynamicColors.applyToActivitiesIfAvailable(getApplication());
+        dummyLogin = findViewById(R.id.dummy_login);
+
+
+        dummyLogin.setOnClickListener(view -> {
+
+        });
+
     }
 }
